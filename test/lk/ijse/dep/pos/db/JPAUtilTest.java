74ab7657b1cfc311e45232c9e416f93305a797a6
Dilini Nadeesha <dilininadeeshawellage@gmail.com>
2020-11-28 03:08:34 +0530

    Initial Commit

---
+++ test/lk/ijse/dep/pos/db/JPAUtilTest.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep.pos.db;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+class JPAUtilTest {
+
+    public static void main(String[] args) {
+//        EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
+//        EntityManager em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//        List resultList = em.createNativeQuery("SELECT id FROM Customer ORDER BY id DESC LIMIT 1").getResultList();
+////        System.out.println(resultList.si);
+//        em.getTransaction().commit();
+//        em.close();
+    }
+
+}
